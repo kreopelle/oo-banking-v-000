ea16a01e74c24febcebb8c3c88af6f008bb9577c
Kayla Reopelle <kreopelle@gmail.com>
2018-05-28 23:43:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
       receiver.deposit(amount)
       @status = "complete"
     elsif valid && @status = "complete"
+      
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
