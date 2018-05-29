3cbba3c2ec7cd7a1e3f1b164a515b1db6af273f3
Kayla Reopelle <kreopelle@gmail.com>
2018-05-29 00:15:21 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   def execute_transaction
     if valid? == false
       @status == "rejected"
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     elsif @status == "complete"
       return
     elsif valid? == true && @status == "pending"
