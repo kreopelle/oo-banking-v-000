295f3fae47e36acfccac7091cc3a3297f2e71ba3
Kayla Reopelle <kreopelle@gmail.com>
2018-05-29 00:00:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-      if valid?
+      if valid? && 
         sender.balance = sender.balance - amount
         receiver.deposit(amount)
         @status = "complete"
